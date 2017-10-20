8536b63134876c2d22de76aea6effdcb3c38af12
zharley18 <zackharley@gmail.com>
2017-10-20 17:31:51 -0400

    Add basic landing page
    Coming soon screen

--- server/controllers/index.controller.js
+++ server/controllers/index.controller.js
@@ -1,7 +1,6 @@
 const _ = require('lodash');
 
 const indexCtrl = {
-    example,
     handleError,
     handlePageNotFound
 };
@@ -10,15 +9,11 @@ module.exports = indexCtrl;
 
 const DEFAULT_ERROR_STATUS_CODE = 500;
 
-function example(req, res, next) {
-    return res.send('This is a server.');
-}
-
 function handleError(err, req, res, next) {
     const status = _.get(err, 'status', DEFAULT_ERROR_STATUS_CODE);
     return res.status(status).send({ message: err.message })
 }
 
 function handlePageNotFound(req, res, next) {
-    return res.status(404).end()
+    return res.redirect('/');
 }
--- server/index.js
+++ server/index.js
@@ -2,11 +2,13 @@ const bodyParser = require('body-parser');
 const config = require('config');
 const express = require('express');
 const routes = require('./routes/index.route');
+const path = require('path');
 
 const app = express();
 const port = config.get('server.port');
 
 app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, '../client')));
 app.use(routes);
 
 app.listen(port, () => console.log(`Hacker dashboard running on port ${port}`));
