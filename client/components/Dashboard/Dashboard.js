433adbe9c433c51599b14c9b00cda6d1db46666f
Zack Harley <zackharley@gmail.com>
2017-11-19 23:38:57 -0500

    Complete application successful template

--- client/components/Dashboard/Dashboard.js
+++ client/components/Dashboard/Dashboard.js
@@ -26,9 +26,9 @@ class Dashboard extends Component {
 					<Segment raised>
 						<Header as='h2'><Emoji text="Congratulations, your application is in! :tada: :heart:" /></Header>
 						<p>Your application to attend QHacks 2018 has been submitted! So, what are the next steps? </p>
-						<p>We will be accepting applications until January 8th 2018, later on that week we will be sending emails to all of the applicants letting them know their application status.</p>
-						<p>If you are accepted, the next step will be to have you RSVP right here in the dashboard, you must complete this step within one week of receiving your acceptance letter. The RSVP confirms that you will be attending and provides us with more information about you to make your experience the best it can be. Some of the information we'll ask you for are things like t-shirt size, dietary restrictions, workshops you are interested in attending, actvities you might be interested in and much more.</p>
-						<p>What if I don't get accepted? We'll have a waiting list that you can enter into from the dashboard and it will be a first come first serve basis. We really wish that we could accept everyone but we receive thousands of applications and just do not have the infrastructure to maintain the quality and safety of our event. Every year there are always those applicants that reach out to us asking if there is anything we can do to help them and our advice is to just make sure you are looking for that email and jump on the waiting list as soon as you can, we can't make any exceptions because it wouldn't be fair.</p>
+						<p> We will be accepting applications until January 8th 2018. Later that week we will be sending emails to all of the applicants updating them on their application status.</p>
+						<p>If you are accepted, your next step will be to RSVP right here in the dashboard. You must complete this step within one week of receiving your acceptance email. The RSVP will be used to confirm your attendance to QHacks 2018.</p>
+						<p>What if I don't get accepted? There's a chance you may be added to the waitlist. If someone who has been accepted fails to RSVP or can't come to the event, then we will fill their spot with someone from the waitlist.</p>
 						<p>While you wait to hear from us there are some things you can do, such as:</p>
 						<ul>
 							<li>Like us on <a href="https://www.facebook.com/QHacks/">Facebook</a></li>
