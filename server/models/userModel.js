504a0dbacf2c32748b0218fefd4dabb4f07a127c
Zack Harley <zackharley@gmail.com>
2018-01-11 14:09:02 -0500

    Fix ESLint test

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -190,7 +190,7 @@ UserSchema.method({
  * @param {Function} next Move to next middleware.
  * @return {Function} Next middleware.
  */
-UserSchema.pre('save', function (next) {
+UserSchema.pre('save', function(next) {
     if (!this.isModified('password')) return next();
 
     bcrypt.genSalt(SALT_WORK_FACTOR, (err, salt) => {
