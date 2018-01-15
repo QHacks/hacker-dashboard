1681a7227ed5b66d88744bbf960720e76aad7d71
Zack Harley <zackharley@gmail.com>
2018-01-15 16:28:50 -0500

    Update user schema
    * Now takes an array of applications for different events
    * RSVP is a field on an application
    * When an application has its status set to ACCEPTED, its RSVP is set to PENDING

--- server/strings/user.strings.js
+++ server/strings/user.strings.js
@@ -1,7 +1,22 @@
 module.exports = {
+    APPLICATION: {
+        STATUSES: {
+            APPLIED: 'APPLIED',
+            REJECTED: 'REJECTED',
+            WAITING_LIST: 'WAITING_LIST',
+            ACCEPTED: 'ACCEPTED'
+        }
+    },
     ROLES: {
         ADMIN: 'ADMIN',
         HACKER: 'HACKER',
         PARTNER: 'PARTNER'
+    },
+    RSVP: {
+        STATUSES: {
+            PENDING: 'PENDING',
+            DECLINED: 'DECLINED',
+            ACCEPTED: 'ACCEPTED'
+        }
     }
 };
