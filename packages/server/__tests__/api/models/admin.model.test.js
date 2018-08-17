4b86ceee847f949bc50f25c2e9e4bc2a8bafcc8b
Joey Tepperman <jmichaelt98@gmail.com>
2018-08-16 21:13:54 -0400

    Add tests to users and associated models

---
+++ packages/server/__tests__/api/models/admin.model.test.js
@@ -0,0 +1,40 @@
+const { Admin } = require("../../../models");
+const { omit } = require("lodash");
+
+describe("Admin model", () => {
+  it("Saves correctly", async () => {
+    const adminObject = {
+      firstName: "admin",
+      lastName: "test",
+      email: "admintest@gmail.com",
+      password: "password",
+      reviewGroup: 3,
+      goldenTickets: 10
+    };
+    const admin = new Admin(adminObject);
+
+    const savedAdmin = await admin.save();
+    const expectedAdmin = omit(savedAdmin.toObject(), [
+      "password",
+      "_id",
+      "createdAt",
+      "modifiedAt",
+      "__v"
+    ]);
+    const actualAdmin = { ...omit(adminObject, ["password"]), role: "ADMIN" };
+
+    expect(actualAdmin).toEqual(expectedAdmin);
+  });
+
+  it("Validates review group", () => {
+    const admin = new Admin({
+      firstName: "admin",
+      lastName: "test",
+      email: "admintest@gmail.com",
+      password: "password",
+      reviewGroup: -1,
+      goldenTickets: 10
+    });
+    return admin.save().catch((err) => expect(err).toBeDefined());
+  });
+});
