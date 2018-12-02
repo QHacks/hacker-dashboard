ea63288c59968b2bc87d6a3722ca60be182d752c
Ross Hill <ross@rosshill.ca>
2018-12-01 22:17:15 -0500

    Wrap dashboard in ContentWrapper

--- packages/client/components/Dashboard/Dashboard.js
+++ packages/client/components/Dashboard/Dashboard.js
@@ -1,24 +1,25 @@
 import React, { Component } from "react";
 import DashboardMenu from "./DashboardMenu";
 import Events from "./DashboardSection/Events";
+import ContentWrapper from "../ContentWrapper/ContentWrapper";
 
 class Dashboard extends Component {
   render() {
     return (
-      <div>
+      <div
+        css={`
+          margin-top: 84px;
+          background: url(${require("../../assets/img/circuits.png")}) no-repeat
+            center bottom;
+          background-size: cover;
+          padding: 40px 0;
+          height: calc(100vh - 84px);
+        `}
+      >
         <DashboardMenu />
-        <div
-          css={`
-              margin-top: 84px;
-              background: url(${require("../../assets/img/circuits.png")}) no-repeat center bottom;
-              background-size: cover;
-              height: calc(100vh - 84px);
-              padding: 50px;
-              width: 100%:
-            `}
-        >
+        <ContentWrapper>
           <Events />
-        </div>
+        </ContentWrapper>
       </div>
     );
   }
